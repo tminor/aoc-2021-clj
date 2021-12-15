7e0874dcb4c1d680c0fbdcae769b9823f4f33e08
Thomas Minor <minort@gmail.com>
2021-12-14 22:15:02 -0500

    Lint day 13

--- src/submarine/day13.clj
+++ src/submarine/day13.clj
@@ -4,18 +4,16 @@
 (defn draw-grid [coordinates]
   (let [max-x (apply max (map first coordinates))
         max-y (apply max (map second coordinates))]
-    (map (fn [y]
-           (println (s/join (map (fn [x] (if (some #(= [x y] %) coordinates) "#" "."))
-                                 (range 0 (inc max-x))))))
+    (map (fn [y] (->> (range 0 (inc max-x))
+                     (map (fn [x] (if (some #(= [x y] %) coordinates) "#" ".")))
+                     s/join
+                     println))
          (range 0 (inc max-y)))))
 
 (defn translate-coordinate [[x y] [tx ty]]
-  (cond
-    (and (pos? ty)
-         (< ty y)) [x (- y (* 2 (- y ty)))]
-    (and (pos? tx)
-         (< tx x)) [(- x (* 2 (- x tx))) y]
-    :else          [x y]))
+  (cond (and (pos? ty) (< ty y)) [x (- y (* 2 (- y ty)))]
+        (and (pos? tx) (< tx x)) [(- x (* 2 (- x tx))) y]
+        :else                    [x y]))
 
 (defn translate-coordinates [coordinates [x y]]
   (map #(translate-coordinate % [x y]) coordinates))
